89bd8ef01b9e2b747a3e580a747bd08b72881b63
Mitchell Sayre <sayremitch@gmail.com>
2023-11-24 22:10:56 -0500

    Finalize schema structure minus resolvers

--- src/schema/post/post.mutation.ts
+++ src/schema/post/post.mutation.ts
@@ -1,27 +1,27 @@
-// import { builder } from "../../builder.js";
-// import { PostObject } from "./post.js";
-// import { Post } from "@prisma/client";
+import { builder } from "../../builder.js";
 // import { createPost, updatePost } from "./post.resolver.js";
-// import { CreatePostInput, UpdatePostInput } from "./post.js";
+import { CreatePostInput, UpdatePostInput } from "./post.js";
 
-// builder.mutationField("createPost", (t) =>
-//   t.field({
-//     type: PostObject,
-//     nullable: true,
-//     args: {
-//       input: t.arg({ type: CreatePostInput, required: true }),
-//     },
-//     resolve: (root, args, ctx) => createPost(args.input),
-//   })
-// );
+builder.mutationField("createPost", (t) =>
+  t.prismaField({
+    type: "Post",
+    nullable: true,
+    args: {
+      input: t.arg({ type: CreatePostInput, required: true }),
+    },
+    resolve: (root, args, ctx) => undefined,
+    // createPost(args.input),
+  })
+);
 
-// builder.mutationField("updatePost", (t) =>
-//   t.field({
-//     type: PostObject,
-//     nullable: true,
-//     args: {
-//       input: t.arg({ type: UpdatePostInput, required: true }),
-//     },
-//     resolve: (root, args, ctx) => updatePost(args.input),
-//   })
-// );
+builder.mutationField("updatePost", (t) =>
+  t.prismaField({
+    type: "Post",
+    nullable: true,
+    args: {
+      input: t.arg({ type: UpdatePostInput, required: true }),
+    },
+    resolve: (root, args, ctx) => undefined,
+    // updatePost(args.input),
+  })
+);
--- src/schema/post/post.ts
+++ src/schema/post/post.ts
@@ -1,4 +1,5 @@
 import { builder } from "../../builder.js";
+import { Post } from "@prisma/client";
 import "./post.query.js";
 import "./post.mutation.js";
 
@@ -8,6 +9,33 @@ builder.prismaObject("Post", {
     title: t.exposeString("title"),
     content: t.exposeString("content"),
     author: t.relation("author"),
+    authorId: t.exposeFloat("authorId"),
     comments: t.relation("comments"),
   }),
 });
+
+type CreatePostInputType = Omit<Post, "id">;
+export const CreatePostInput =
+  builder.inputRef<CreatePostInputType>("CreatePostInput");
+CreatePostInput.implement({
+  fields: (t) => ({
+    title: t.string({ required: true }),
+    content: t.string({ required: true }),
+    authorId: t.float({ required: true }),
+  }),
+});
+export type CreatePostInputShape = typeof CreatePostInput.$inferInput;
+
+type UpdatePostInputType = Required<Pick<Post, "id">> &
+  Partial<Omit<Post, "id">>; // TODO: Make this cleaner
+export const UpdatePostInput =
+  builder.inputRef<UpdatePostInputType>("UpdatePostInput");
+UpdatePostInput.implement({
+  fields: (t) => ({
+    id: t.int({ required: true }),
+    title: t.string(),
+    content: t.string(),
+    authorId: t.float(),
+  }),
+});
+export type UpdatePostInputShape = typeof UpdatePostInput.$inferInput;
