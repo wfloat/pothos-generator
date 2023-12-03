04894083ee2b6a221627121ca80429530059c6c9
Mitchell Sayre <sayremitch@gmail.com>
2023-12-03 15:52:06 -0500

    Add working data loaders for single object queries

--- Comment.sql
+++ Comment.sql
@@ -1,41 +1,33 @@
--- Comments for Post 11
+-- Comments for Post "64d451a9-1de3-4ffe-8791-7c7ff904bad8" (Majestic Elephants)
 INSERT INTO "Comment" ("comment", "authorId", "postId")
-VALUES ('Incredible facts about elephants!', 5, 11);
+VALUES ('Incredible facts about elephants!', '65ed0c42-d62e-4c6a-a070-754245bdb630', '64d451a9-1de3-4ffe-8791-7c7ff904bad8'); -- Vito Buckhurst
 
--- Comments for Post 12
+-- Comments for Post "54b66417-c911-4ea1-9ddd-a26fc0c95728" (Cheetahs: Fastest Land Animals)
 INSERT INTO "Comment" ("comment", "authorId", "postId")
-VALUES ('I never knew cheetahs were that fast!', 3, 12);
+VALUES ('I never knew cheetahs were that fast!', 'c3c5660b-55f3-4d0c-a593-f8ec37d4745d', '54b66417-c911-4ea1-9ddd-a26fc0c95728'); -- Lorens Pretty
 INSERT INTO "Comment" ("comment", "authorId", "postId")
-VALUES ('Their speed is absolutely astounding.', 8, 12);
+VALUES ('Their speed is absolutely astounding.', '9efd575c-d761-4f27-9cc8-7475b6fcb7ad', '54b66417-c911-4ea1-9ddd-a26fc0c95728'); -- Andros Chupin
 
--- Comments for Post 13
+-- Comments for Post "eeb6af6d-f902-46e2-a72c-bf9f04b5c9e4" (The Blue Whale)
 INSERT INTO "Comment" ("comment", "authorId", "postId")
-VALUES ('Blue whales are truly magnificent.', 4, 13);
+VALUES ('Blue whales are truly magnificent.', 'bd131796-fa69-4d91-8c43-9cbdb6a54e39', 'eeb6af6d-f902-46e2-a72c-bf9f04b5c9e4'); -- Ardelia Arnoult
 
--- Comments for Post 14
+-- Comments for Post "2babf636-3aed-43d2-a9a4-441b91f7a661" (Fascinating Octopuses)
 -- No comments for this post
 
--- Comments for Post 15
+-- Comments for Post "39ff231c-0297-4c8b-ad7f-aaeb5b81ccec" (Giraffes: The Tall Mammals)
 INSERT INTO "Comment" ("comment", "authorId", "postId")
-VALUES ('I love giraffes!', 2, 15);
+VALUES ('I love giraffes!', '530b1887-bf68-44d5-9a4d-c89238a7d4d7', '39ff231c-0297-4c8b-ad7f-aaeb5b81ccec'); -- Ferdy Vicary
 INSERT INTO "Comment" ("comment", "authorId", "postId")
-VALUES ('Their height is really something special.', 9, 15);
+VALUES ('Their height is really something special.', '9eeb0526-c70d-4d1f-b6ee-cf259e35de0c', '39ff231c-0297-4c8b-ad7f-aaeb5b81ccec'); -- Johnath Rowaszkiewicz
 
--- Comments for Post 16
+-- Comments for Post "7d3ec71b-b080-42ef-ab2e-a9baeacf16f1" (Penguins in Antarctica)
 -- No comments for this post
 
--- Comments for Post 17
+-- Comments for Post "d4127dc8-860f-4bc3-843f-a99818f08c6c" (Mysterious Jellyfish)
 INSERT INTO "Comment" ("comment", "authorId", "postId")
-VALUES ('The roar of a lion is indeed very powerful.', 7, 17);
-
--- Comments for Post 18
+VALUES ('Jellyfish are so mysterious and fascinating.', '0adfd653-b1ac-4d12-aa80-1d77aeaa1344', 'd4127dc8-860f-4bc3-843f-a99818f08c6c'); -- Claybourne Tatford
 INSERT INTO "Comment" ("comment", "authorId", "postId")
-VALUES ('Jellyfish are so mysterious and fascinating.', 6, 18);
+VALUES ('They are such ancient creatures!', '3b7878c4-e41f-4837-9426-54b59aa83913', 'd4127dc8-860f-4bc3-843f-a99818f08c6c'); -- Emiline Brasier
 INSERT INTO "Comment" ("comment", "authorId", "postId")
-VALUES ('They are such ancient creatures!', 1, 18);
-INSERT INTO "Comment" ("comment", "authorId", "postId")
-VALUES ('Their survival through ages is remarkable.', 12, 18);
-
--- Comments for Post 19
-INSERT INTO "Comment" ("comment", "authorId", "postId")
-VALUES ('Chameleons are my favorite!', 10, 19);
+VALUES ('Their survival through ages is remarkable.', 'f10fa015-ad3e-45f6-a1b2-2d4583b07173', 'd4127dc8-860f-4bc3-843f-a99818f08c6c'); -- Erma
