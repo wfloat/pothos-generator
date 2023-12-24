d9be0aa66eafb4de808f58817c2d17ad1245a260
Mitchell Sayre <sayremitch@gmail.com>
2023-12-23 22:57:13 -0500

    Add support for optionals on update mutation input

---
+++ example/helpers.ts
@@ -0,0 +1,22 @@
+type UndefinedPartial<T> = {
+  [K in keyof T]?: T[keyof T] extends null ? null : T[K];
+};
+
+export function removeNullFieldsThatAreNonNullable<T extends object>(
+  input: Record<string, T[keyof T]>,
+  nullability: { [K in keyof T]?: boolean | undefined }
+): UndefinedPartial<T> {
+  const keysToRemove: (keyof T)[] = [];
+
+  for (const key in input) {
+    if (input[key] === null && nullability[key as keyof T] !== true) {
+      keysToRemove.push(key as keyof T);
+    }
+  }
+
+  for (const key of keysToRemove) {
+    delete input[key as string];
+  }
+
+  return input as UndefinedPartial<T>;
+}
--- example/schema/account/account.mutation.ts
+++ example/schema/account/account.mutation.ts
@@ -1,27 +1,75 @@
 import { builder } from "../../builder.js";
-import { CreateAccountInput, UpdateAccountInput } from "./account.js";
-// import { createAccount, updateAccount } from "./account.resolver.js";
+import { db } from "../../database.js";
+import { Account } from "@prisma/client";
+import { removeNullFieldsThatAreNonNullable } from "../../helpers.js";
+// import { CreateAccountInput, UpdateAccountInput, UpdateAccountInputShape } from "./account.js";
+
+type CreateAccountInputType = Omit<Account, "id">;
+export const CreateAccountInput =
+  builder.inputRef<CreateAccountInputType>("CreateAccountInput");
+CreateAccountInput.implement({
+  fields: (t) => ({
+    firstName: t.string({ required: true }),
+    lastName: t.string({ required: true }),
+  }),
+});
+export type CreateAccountInputShape = typeof CreateAccountInput.$inferInput;
 
 builder.mutationField("createAccount", (t) =>
-t.prismaField({
+  t.prismaField({
     type: "Account",
-    nullable: true,
+    nullable: false,
     args: {
-    input: t.arg({ type: CreateAccountInput, required: true }),
+      input: t.arg({ type: CreateAccountInput, required: true }),
     },
-    resolve: (root, args, context, info) => undefined,
-    // createAccount(args.input),
-})
+    resolve: async (query, parent, args, context, info) => {
+      const result = await db
+        .insertInto("Account")
+        .values(args.input)
+        .returning(["id"])
+        .executeTakeFirstOrThrow();
+      return context.loaders.account.load(result.id);
+    },
+  })
 );
 
+type UpdateAccountInputType = Required<Pick<Account, "id">> &
+  Partial<Omit<Account, "id">>; // TODO: Make this cleaner
+export const UpdateAccountInput =
+  builder.inputRef<UpdateAccountInputType>("UpdateAccountInput");
+UpdateAccountInput.implement({
+  fields: (t) => ({
+    id: t.id({ required: true }),
+    firstName: t.string(),
+    lastName: t.string(),
+  }),
+});
+export type UpdateAccountInputShape = typeof UpdateAccountInput.$inferInput;
+
+const AccountNullability: { [K in keyof Account]?: boolean } = {
+  firstName: false,
+  lastName: true,
+};
+
 builder.mutationField("updateAccount", (t) =>
-t.prismaField({
+  t.prismaField({
     type: "Account",
-    nullable: true,
+    nullable: false,
     args: {
-    input: t.arg({ type: UpdateAccountInput, required: true }),
+      input: t.arg({ type: UpdateAccountInput, required: true }),
     },
-    resolve: (root, args, context, info) => undefined,
-    // updateAccount(args.input),
-})
+    resolve: async (query, parent, args, context, info) => {
+      const input = removeNullFieldsThatAreNonNullable<Account>(
+        args.input,
+        AccountNullability
+      );
+
+      const result = await db
+        .updateTable("Account")
+        .set(input)
+        .where("id", "=", args.input.id)
+        .executeTakeFirst();
+      return context.loaders.account.load(args.input.id);
+    },
+  })
 );
